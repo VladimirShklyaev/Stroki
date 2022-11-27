166ad8ab86ed5ba02833fdc8f24bd4d312f14099
VladimirShklyaev <107680299+VladimirShklyaev@users.noreply.github.com>
2022-11-27 15:19:17 +0300

    Add files via upload

---
+++ 8-1/8-1.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+int proverka(string vvod)//поиск наибольшей последовательности чисел в строке
+{
+    int l = 0;
+    int maxlen = 0;
+    for (int i = 0;i < vvod.length();i++)
+    {
+        if (vvod[i] >= '0' && vvod[i] <= '9')
+        {
+            l++;
+        }
+        else
+        {
+            l = 0;
+        }
+        if (l > maxlen)
+        {
+            maxlen = l;
+        }
+    }
+    return maxlen;
+}
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    using namespace std;
+    string vvod;
+    cout << "Введите строку" << endl;
+    cin >> vvod;
+    cout<<"Максимальная длинна равна = " << proverka(vvod) << endl;
+}
